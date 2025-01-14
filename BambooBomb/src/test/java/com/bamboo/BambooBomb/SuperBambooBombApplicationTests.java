36ffae4e3ef57c9d8226406fd13b1d732e2ffcb6
hamon1 <leejh021122@naver.com>
2025-01-14 19:48:30 +0900

    init

---
+++ BambooBomb/src/test/java/com/bamboo/BambooBomb/SuperBambooBombApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bamboo.BambooBomb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperBambooBombApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
