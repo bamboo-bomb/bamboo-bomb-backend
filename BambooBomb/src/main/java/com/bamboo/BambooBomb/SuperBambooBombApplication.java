0ae6fd398e46faac82704f2e54ef0fdce6ce5b1b
hamon1 <leejh021122@naver.com>
2025-01-14 20:56:10 +0900

    init

--- BambooBomb/src/main/java/com/bamboo/BambooBomb/SuperBambooBombApplication.java
+++ BambooBomb/src/main/java/com/bamboo/BambooBomb/SuperBambooBombApplication.java
@@ -3,10 +3,16 @@ package com.bamboo.BambooBomb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class SuperBambooBombApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+
+        System.setProperty("CLIENT_ID", dotenv.get("CLIENT_ID"));
+
 		SpringApplication.run(SuperBambooBombApplication.class, args);
 	}
 
