ed15d2ab35cadc5de3189cd409725e823070cc4d
hamon1 <leejh021122@naver.com>
2025-01-19 02:30:04 +0900

    feat: 제목, 본문 길이 제한(각 20, 1000)

--- BambooBomb/src/main/java/com/bamboo/BambooBomb/controller/PostController.java
+++ BambooBomb/src/main/java/com/bamboo/BambooBomb/controller/PostController.java
@@ -7,6 +7,7 @@ import com.bamboo.BambooBomb.model.Post;
 import com.bamboo.BambooBomb.service.PostService;
 import com.bamboo.BambooBomb.dto.PostRequest;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,7 +24,15 @@ public class PostController {
     }
 
     @PostMapping
-    public ResponseEntity<Post> createPost(@RequestBody PostRequest postRequest) {
+    public ResponseEntity<?> createPost(@RequestBody PostRequest postRequest) {
+        // 제목과 내용의 길이 제한 검증
+        if (postRequest.getTitle().length() > 20) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Title must not exceed 20 characters.");
+        }
+        if (postRequest.getContent().length() > 1000) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Content must not exceed 1000 characters.");
+        }
+
         Post createdPost = postService.createPost(postRequest.getTitle(), postRequest.getContent(), postRequest.getUserId());
         return ResponseEntity.ok(createdPost);
     }
